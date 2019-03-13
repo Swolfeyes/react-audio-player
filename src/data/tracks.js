f3d53fc5f5159d5cf510ed05e4f8a87a6fe48356
Stefania <s.occhilupo87@gmail.com>
2019-03-13 07:56:14 +0000

    lad mp3 in production

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -1,26 +1,22 @@
-import track1 from '../assets/tracks/8in8_-_05_-_Ill_Be_My_Mirror.mp3';
-import track2 from '../assets/tracks/Dead_Combo_-_01_-_Povo_Que_Cas_Descalo.mp3';
-import track3 from '../assets/tracks/Dot_-_05_-_IMF.mp3';
-
 export const tracks = [
     {
         title: `I'll be my mirror`,
         artist: '8in8',
-        track: track1,
+        track: './src/assets/tracks/8in8_-_05_-_Ill_Be_My_Mirror.mp3',
         image: 'lotus',
         id:'22'
     },
     {
         title: 'Provo que cas descalo',
         artist: 'Combo',
-        track: track2,
+        track: './src/assets/tracks/Dead_Combo_-_01_-_Povo_Que_Cas_Descalo.mp3',
         image: 'flower',
         id: '333'
     },
     {
         title: 'IMF',
         artist: 'Dot',
-        track: track3,
+        track: './src/assets/tracks/Dot_-_05_-_IMF.mp3',
         image: 'lake',
         id: '4444',
     },
